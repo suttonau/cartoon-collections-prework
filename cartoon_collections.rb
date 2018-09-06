0ea805f9fbd6d3c8bac6c9b0fd3f540ed3658c71
Sean Sutton Jr <sutton_mjbc@icloud.com>
2018-09-06 17:10:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
   
   if strings.any? {|cheese| cheese_types.include?(cheese)} == true
-    string.find {|x| cheese_types.find{|y| y == x}}
+    strings.find {|x| cheese_types.find{|y| y == x}}
   else
     puts "No cheese"
   
