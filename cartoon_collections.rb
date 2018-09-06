5cae151bcab742cc68ccdf472d5e09fe9bb83242
Sean Sutton Jr <sutton_mjbc@icloud.com>
2018-09-06 17:05:54 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.collect do |call|
+    call[0] = call.capitalize
+    call << "!"
+  end 
+    calls
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  words.any? do |word|
+    word.length > 4
+  end 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  strings.find do |x|
+   cheese_types.include?(x)
+  end 
 end
